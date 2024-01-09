9d805fb09f8b111b0f683425519bae3f00cd107a
AL <alexl5105@mail.ru>
2024-01-08 17:13:56 -0800

    Initial Commit

---
+++ Missanger/MissangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissangerApp.swift
+//  Missanger
+//
+//  Created by Alexey Larionov on 08.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MissangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
